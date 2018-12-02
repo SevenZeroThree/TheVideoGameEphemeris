d4262bc8058c401dac06be62718265305501a75a
Christopher Tobin <1302813+christophertobin@users.noreply.github.com>
2018-12-01 23:01:05 -0500

    add key to prevent warning

--- src/scripts/mass-effect-3/components/characterClassList.js
+++ src/scripts/mass-effect-3/components/characterClassList.js
@@ -5,7 +5,7 @@ import CharacterClassListItem from './characterClassListItem';
 function CharacterClassList(props) {
     const characters = props.characters;
     const listOfCharacters = characters.map((character) =>
-      <CharacterClassListItem character={character} />
+      <CharacterClassListItem key={character.name} character={character} />
     );
 
     return (
